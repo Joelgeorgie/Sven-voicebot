225e10fe570d085a69354b7985fafa16f6fab065
JoelGeorgie <joelvgeorgie@gmail.com>
2023-07-28 17:52:00 +0530

    first one

---
+++ src/components/Email.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Email = () => {
+  return (
+    <div className=' flex-col justify-between text-whitemax-w-[1240px] mx-auto '>
+        <p className='w-[60%] mx-auto text'>
+            SEND WITHOUT NO CREDENTIALS
+        </p>
+    </div>
+  )
+}
+
+export default Email
