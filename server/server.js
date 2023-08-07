4922b257b9bbfcc4aadb2d27bf5e156614379803
JoelGeorgie <joelvgeorgie@gmail.com>
2023-08-07 20:35:55 +0530

    removing API_KEY

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app=express();
 app.use(express.json())
 app.use(cors())
 
-const API_KEY= "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"
+const API_KEY= process.env.API_KEY
 
 
 app.post('/completions',async (req,res)=>{
